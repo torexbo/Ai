07de65d486d08ceb25c7479a43114f5414a122b6
torexbo <98379781+torexbo@users.noreply.github.com>
2024-05-22 16:22:14 +0600

    Update Artcon.bash

--- Artcon.bash
+++ Artcon.bash
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "Initial commit"
 git branch -M main
-git remote add origin https://github.com/yourusername/my-poster-generator.git
+git remote add origin https://github.com/torexbo/Ai.git
 git push -u origin main
